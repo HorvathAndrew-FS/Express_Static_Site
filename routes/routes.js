23f72a3ab20ff7ba183bc16c905e58d65c29b4c9
HorvathAndrew-FS <skwerl@student.fullsail.edu>
2022-10-10 00:35:42 -0500

    fixing routing

--- routes/routes.js
+++ routes/routes.js
@@ -8,10 +8,10 @@ router.get('/', (req, res) => {
 
 router.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, '..', '/views/about.html'));
-})
+});
 
 router.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, '..', '/views/services.html'));
-})
+});
 
 module.exports = router;
