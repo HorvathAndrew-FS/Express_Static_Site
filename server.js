67010cbea8ad37df5f9af687cf231a526fc1419e
HorvathAndrew-FS <skwerl@student.fullsail.edu>
2022-10-09 22:08:35 -0500

    following along doing spaghetti code

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-const http - require('http');
+const http = require('http');
 require('dotenv').config();
+const express = require('express');
+
+// request listener
+const app = express();
+app.get('/', (req, res) => {
+    res.send("This is the wrong way...");
+});
+
+const router = express.Router();
+
+http.createServer(app).listen(process.env.port, () => {
+    console.log(`Server is running on port: ${process.env.port}`);
+});
